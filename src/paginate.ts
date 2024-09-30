6169f17d29f186a68ff98bd404fc8bc4ae78e171
Mohd Ahmad <61076339+MohdAhmad1@users.noreply.github.com>
2024-09-30 18:55:47 +0530

    refactor 2

--- src/paginate.ts
+++ src/paginate.ts
@@ -27,13 +27,11 @@ export async function paginate<T extends ObjectLiteral>(
     repo: Repository<T> | SelectQueryBuilder<T>,
     config: PaginateConfig<T>
 ): Promise<Paginated<T>> {
-    const queryBuilder = NestJsPaginate.initialize(repo, config)
+    const queryBuilder = new NestJsPaginate(repo, query, config)
 
-    NestJsPaginate.applyFilters(query, queryBuilder, config)
-    NestJsPaginate.applySorting(query, queryBuilder, config)
-    NestJsPaginate.applyColumnsSelection(query, queryBuilder, config)
-    NestJsPaginate.applyPagination(query, queryBuilder, config)
-    return NestJsPaginate.getPaginatedResponse(query, queryBuilder, config)
+    queryBuilder.applyColumnsSelection().applyFilters().applyPagination().applySorting()
+
+    return queryBuilder.getPaginatedResponse()
 }
 
 function getPaginationLimit<T extends ObjectLiteral>(
@@ -61,8 +59,20 @@ function getPaginationLimit<T extends ObjectLiteral>(
     return Math.min(query.limit ?? defaultLimit, maxLimit)
 }
 
-export class NestJsPaginate {
-    static initialize<T extends ObjectLiteral>(repo: Repository<T> | SelectQueryBuilder<T>, config: PaginateConfig<T>) {
+export class NestJsPaginate<T extends ObjectLiteral> {
+    private config: PaginateConfig<T>
+    private query: PaginateQuery
+    private queryBuilder: SelectQueryBuilder<T>
+
+    constructor(repo: Repository<T> | SelectQueryBuilder<T>, query: PaginateQuery, config: PaginateConfig<T>) {
+        const queryBuilder = this.initialize(repo, config)
+
+        this.config = config
+        this.query = query
+        this.queryBuilder = queryBuilder
+    }
+
+    private initialize(repo: Repository<T> | SelectQueryBuilder<T>, config: PaginateConfig<T>) {
         const queryBuilder = isRepository(repo) ? repo.createQueryBuilder('__root') : repo
 
         if (isRepository(repo) && !config.relations && config.loadEagerRelations === true) {
@@ -114,144 +124,133 @@ export class NestJsPaginate {
         return queryBuilder
     }
 
-    static applyColumnsSelection<T extends ObjectLiteral>(
-        query: PaginateQuery,
-        queryBuilder: SelectQueryBuilder<T>,
-        config: PaginateConfig<T>
-    ) {
+    applyColumnsSelection() {
         let selectParams =
-            config.select && query.select && !config.ignoreSelectInQueryParam
-                ? config.select.filter((column) => query.select.includes(column))
-                : config.select
+            this.config.select && this.query.select && !this.config.ignoreSelectInQueryParam
+                ? this.config.select.filter((column) => query.select.includes(column))
+                : this.config.select
 
-        if (!includesAllPrimaryKeyColumns(queryBuilder, query.select)) {
-            selectParams = config.select
+        if (!includesAllPrimaryKeyColumns(this.queryBuilder, this.query.select)) {
+            selectParams = this.config.select
         }
 
         const cols: string[] = selectParams.reduce((cols, currentCol) => {
             const columnProperties = getPropertiesByColumnName(currentCol)
-            const isRelation = checkIsRelation(queryBuilder, columnProperties.propertyPath)
-            cols.push(fixColumnAlias(columnProperties, queryBuilder.alias, isRelation))
+            const isRelation = checkIsRelation(this.queryBuilder, columnProperties.propertyPath)
+            cols.push(fixColumnAlias(columnProperties, this.queryBuilder.alias, isRelation))
             return cols
         }, [])
 
-        queryBuilder.select(cols)
+        this.queryBuilder.select(cols)
 
-        return queryBuilder
+        return this
     }
 
-    static applyFilters<T extends ObjectLiteral>(
-        query: PaginateQuery,
-        queryBuilder: SelectQueryBuilder<T>,
-        config: PaginateConfig<T>
-    ) {
-        if (query.filter) {
-            addFilter(queryBuilder, query, config.filterableColumns)
+    applyFilters() {
+        if (this.query.filter) {
+            addFilter(this.queryBuilder, this.query, this.config.filterableColumns)
         }
 
-        return queryBuilder
+        return this
     }
 
-    static applySorting<T extends ObjectLiteral>(
-        query: PaginateQuery,
-        queryBuilder: SelectQueryBuilder<T>,
-        config: PaginateConfig<T>
-    ) {
-        const dbType = queryBuilder.connection.options.type
+    applySorting() {
+        const dbType = this.queryBuilder.connection.options.type
         const isMariaDbOrMySql = (dbType: string) => dbType === 'mariadb' || dbType === 'mysql'
         const isMMDb = isMariaDbOrMySql(dbType)
 
         const sortBy = [] as SortBy<T>
 
         let nullSort: string | undefined
-        if (config.nullSort) {
+
+        if (this.config.nullSort) {
             if (isMMDb) {
-                nullSort = config.nullSort === 'last' ? 'IS NULL' : 'IS NOT NULL'
+                nullSort = this.config.nullSort === 'last' ? 'IS NULL' : 'IS NOT NULL'
             } else {
-                nullSort = config.nullSort === 'last' ? 'NULLS LAST' : 'NULLS FIRST'
+                nullSort = this.config.nullSort === 'last' ? 'NULLS LAST' : 'NULLS FIRST'
             }
         }
 
-        if (config.sortableColumns.length < 1) {
+        if (this.config.sortableColumns.length < 1) {
             const message = "Missing required 'sortableColumns' config."
             logger.debug(message)
             throw new ServiceUnavailableException(message)
         }
 
-        if (query.sortBy) {
-            for (const order of query.sortBy) {
-                if (isEntityKey(config.sortableColumns, order[0]) && ['ASC', 'DESC'].includes(order[1])) {
+        if (this.query.sortBy) {
+            for (const order of this.query.sortBy) {
+                if (isEntityKey(this.config.sortableColumns, order[0]) && ['ASC', 'DESC'].includes(order[1])) {
                     sortBy.push(order as Order<T>)
                 }
             }
         }
 
         if (!sortBy.length) {
-            sortBy.push(...(config.defaultSortBy || [[config.sortableColumns[0], 'ASC']]))
+            sortBy.push(...(this.config.defaultSortBy || [[this.config.sortableColumns[0], 'ASC']]))
         }
 
         for (const order of sortBy) {
             const columnProperties = getPropertiesByColumnName(order[0])
-            const { isVirtualProperty } = extractVirtualProperty(queryBuilder, columnProperties)
-            const isRelation = checkIsRelation(queryBuilder, columnProperties.propertyPath)
-            const isEmbeded = checkIsEmbedded(queryBuilder, columnProperties.propertyPath)
+            const { isVirtualProperty } = extractVirtualProperty(this.queryBuilder, columnProperties)
+            const isRelation = checkIsRelation(this.queryBuilder, columnProperties.propertyPath)
+            const isEmbeded = checkIsEmbedded(this.queryBuilder, columnProperties.propertyPath)
 
-            let alias = fixColumnAlias(columnProperties, queryBuilder.alias, isRelation, isVirtualProperty, isEmbeded)
+            let alias = fixColumnAlias(
+                columnProperties,
+                this.queryBuilder.alias,
+                isRelation,
+                isVirtualProperty,
+                isEmbeded
+            )
 
             if (isMMDb) {
                 if (isVirtualProperty) {
                     alias = `\`${alias}\``
                 }
                 if (nullSort) {
-                    queryBuilder.addOrderBy(`${alias} ${nullSort}`)
+                    this.queryBuilder.addOrderBy(`${alias} ${nullSort}`)
                 }
-                queryBuilder.addOrderBy(alias, order[1])
+                this.queryBuilder.addOrderBy(alias, order[1])
             } else {
                 if (isVirtualProperty) {
                     alias = `"${alias}"`
                 }
-                queryBuilder.addOrderBy(alias, order[1], nullSort as 'NULLS FIRST' | 'NULLS LAST' | undefined)
+                this.queryBuilder.addOrderBy(alias, order[1], nullSort as 'NULLS FIRST' | 'NULLS LAST' | undefined)
             }
         }
+
+        return this
     }
 
-    static applyPagination<T extends ObjectLiteral>(
-        query: PaginateQuery,
-        queryBuilder: SelectQueryBuilder<T>,
-        config: PaginateConfig<T>
-    ) {
-        const page = positiveNumberOrDefault(query.page, 1, 1)
+    applyPagination() {
+        const page = positiveNumberOrDefault(this.query.page, 1, 1)
 
-        const maxLimit = config.maxLimit || PaginationLimit.DEFAULT_MAX_LIMIT
+        const maxLimit = this.config.maxLimit || PaginationLimit.DEFAULT_MAX_LIMIT
 
         const isPaginated = !(
-            query.limit === PaginationLimit.COUNTER_ONLY ||
-            (query.limit === PaginationLimit.NO_PAGINATION && maxLimit === PaginationLimit.NO_PAGINATION)
+            this.query.limit === PaginationLimit.COUNTER_ONLY ||
+            (this.query.limit === PaginationLimit.NO_PAGINATION && maxLimit === PaginationLimit.NO_PAGINATION)
         )
 
-        const limit = getPaginationLimit(query, true, config)
+        const limit = getPaginationLimit(this.query, true, this.config)
 
-        if (!isPaginated) return queryBuilder
+        if (!isPaginated) return this
 
         // Allow user to choose between limit/offset and take/skip.
         // However, using limit/offset can cause problems when joining one-to-many etc.
-        if (config.paginationType === PaginationType.LIMIT_AND_OFFSET) {
-            queryBuilder.limit(limit).offset((page - 1) * limit)
+        if (this.config.paginationType === PaginationType.LIMIT_AND_OFFSET) {
+            this.queryBuilder.limit(limit).offset((page - 1) * limit)
         } else {
-            queryBuilder.take(limit).skip((page - 1) * limit)
+            this.queryBuilder.take(limit).skip((page - 1) * limit)
         }
 
-        return queryBuilder
+        return this
     }
 
-    static async getPaginatedResponse<T extends ObjectLiteral>(
-        query: PaginateQuery,
-        queryBuilder: SelectQueryBuilder<T>,
-        config: PaginateConfig<T>
-    ): Promise<Paginated<T>> {
+    async getPaginatedResponse(): Promise<Paginated<T>> {
         const response = new Paginated<T>()
 
-        response.data = await queryBuilder.getMany()
+        response.data = await this.queryBuilder.getMany()
 
         return response
     }
